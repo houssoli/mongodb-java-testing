4edb8513ff660cf63db87bc48fb2229984a4a404
Micha Kops <micha.kops@gmail.com>
2013-10-14 21:41:21 +0200

    Example persisting a book collection added.

--- src/test/java/it/MongoDbIntegrationTest.java
+++ src/test/java/it/MongoDbIntegrationTest.java
@@ -1,6 +1,7 @@
 package it;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.util.UUID;
 
@@ -18,6 +19,11 @@ import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 
 public class MongoDbIntegrationTest {
+	private static final String BOOK2_AUTHOR = "Vaughn Vernon";
+	private static final String BOOK2_TITLE = "Implementing Domain Driven Design";
+	private static final String BOOK1_AUTHOR = "Tim Tester";
+	private static final String BOOK1_TITLE = "Some book";
+
 	MongodForTestsFactory factory;
 	MongoClient mongo;
 
@@ -34,24 +40,31 @@ public class MongoDbIntegrationTest {
 	}
 
 	@Test
-	public void testPretechAddressPersistance() {
+	public void shouldPersistAndFindBooks() {
 		DB db = mongo.getDB("test-" + UUID.randomUUID());
-		DBCollection collection = db.createCollection("testCol",
+		DBCollection collection = db.createCollection("bookCollection",
 				new BasicDBObject());
-		BasicDBObject contact = new BasicDBObject();
-		contact.put("name", "pretech");
-		contact.put("address", "bangalore");
-		// Inserting document
-		collection.insert(contact);
-		DBCursor cursorDoc = collection.find();
-		BasicDBObject contact1 = new BasicDBObject();
-		while (cursorDoc.hasNext()) {
-			contact1 = (BasicDBObject) cursorDoc.next();
-			System.out.println(contact1);
-		}
+		BasicDBObject book1 = new BasicDBObject();
+		book1.put("title", BOOK1_TITLE);
+		book1.put("author", BOOK1_AUTHOR);
 
-		assertEquals(contact1.get("name"), "pretech1");
+		BasicDBObject book2 = new BasicDBObject();
+		book2.put("title", BOOK2_TITLE);
+		book2.put("author", BOOK2_AUTHOR);
 
+		collection.insert(book1);
+		collection.insert(book2);
+
+		DBCursor docCursor = collection.find();
+		assertThat(docCursor.size(), equalTo(2));
+
+		BasicDBObject bookFromDb1 = (BasicDBObject) docCursor.next();
+		assertThat((String) bookFromDb1.get("title"), equalTo(BOOK1_TITLE));
+		assertThat((String) bookFromDb1.get("author"), equalTo(BOOK1_AUTHOR));
+
+		BasicDBObject bookFromDb2 = (BasicDBObject) docCursor.next();
+		assertThat((String) bookFromDb2.get("title"), equalTo(BOOK2_TITLE));
+		assertThat((String) bookFromDb2.get("author"), equalTo(BOOK2_AUTHOR));
 	}
 
 }
